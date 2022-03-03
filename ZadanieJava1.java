5447e559d2efbfca05bf66a671b7bfecc80a7f98
Grzegorz Ko <giantcaterpillar@gmail.com>
2022-03-03 17:39:25 +0100

    initial commit

---
+++ ZadanieJava1.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ZadanieJava1 {
+//Guess number game
+
+    public static void main(String[] args) {
+        welcomePlayer();
+        playTheGameLoop();
+    }
+
+    private static void welcomePlayer() {
+        System.out.println("\n");
+        System.out.println("Welcome to the Game!");
+        System.out.println("Your task is to guess the number between 0 and 99 in 5 tries.");
+    }
+
+    private static void printTriesLeft(int numberOfTriesLeft) {
+        if (numberOfTriesLeft > 0) {
+            System.out.println("You have " + numberOfTriesLeft + " tries left.");
+            System.out.println("Please try again");
+        } else {
+            System.out.println("This was your last try.");
+        }
+    }
+
+    private static boolean checkTriesLeft(int numberOfTriesLeft) {
+        return numberOfTriesLeft > 0;
+    }
+
+    private static void playTheGameLoop() {
+        int numberToGuess = new Random().nextInt(100);
+        int triesLeft = 5;
+
+        Scanner scanner = new Scanner(System.in);
+        boolean winner = false;
+
+        while (checkTriesLeft(triesLeft)) {
+
+            System.out.println("\n");
+            System.out.println("Try to guess a number between 0 and 99");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not a number! Please stick to values between 0 and 99");
+                scanner.next();
+            }
+
+            int attemptedNumber = scanner.nextInt();
+
+            if (attemptedNumber >= 0 && attemptedNumber < 100) {
+                triesLeft--;
+
+                if (attemptedNumber > numberToGuess) {
+                    System.out.println("Your number is GREATER than the one you are trying to guess");
+
+                    if (checkTriesLeft(triesLeft)) {
+                        printTriesLeft(triesLeft);
+                        continue;
+                    } else printTriesLeft(triesLeft);
+                    break;
+                }
+
+                if (attemptedNumber < numberToGuess) {
+                    System.out.println("Your number is LOWER than the one you are trying to guess");
+
+                    if (checkTriesLeft(triesLeft)) {
+                        printTriesLeft(triesLeft);
+                        continue;
+                    } else printTriesLeft(triesLeft);
+                    break;
+                }
+
+                System.out.println("You guessed it!");
+                winner = true;
+                break;
+
+            } else {
+                System.out.println("Number incorrect. Please stick to values between 0 and 99");
+            }
+        }
+        if (!winner) {
+            System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
+        }
+    }
+}
---
+++ ZadanieJava2.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZadanieJava2 {
+
+//Arrays creator
+
+    public static void main(String[] args) {
+
+        int[] numbers = createArray();
+        fillArray(numbers);
+        double average = calculateAverage(numbers);
+        int highest = calculateHighest(numbers);
+        printAverageAndHighest(average, highest);
+    }
+
+    private static int[] createArray() {
+
+        System.out.println("Please insert required array size:");
+        Scanner scanner = new Scanner(System.in);
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("That's not correct a number!");
+            scanner.next();
+        }
+
+        int size = scanner.nextInt();
+        return new int[size];
+    }
+
+
+    private static void fillArray(int[] numbers) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You will now be asked to insert values (whole numbers) to each of the cells of array");
+
+        int currentCell = 0;
+
+        for (int ignored : numbers) {
+            int displayedCurrentCell = currentCell + 1;
+            System.out.println("Please insert value for cell " + displayedCurrentCell + " out of " + numbers.length);
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not a number!");
+                scanner.next();
+            }
+
+            numbers[currentCell] = scanner.nextInt();
+            currentCell++;
+        }
+    }
+
+    private static double calculateAverage(int[] numbers) {
+
+        double sumOfValues = 0.00;
+
+        for (int x : numbers) {
+            sumOfValues += x;
+        }
+
+
+        return sumOfValues / numbers.length;
+    }
+
+    private static int calculateHighest(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1];
+    }
+
+    private static void printAverageAndHighest(double avg, int high) {
+        System.out.println("Average value in created array was = " + avg);
+        System.out.println("Highest value insterted into array was = " + high);
+    }
+}
